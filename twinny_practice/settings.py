c4ed52c87e14bc094d63714d65590fc7234e70b2
jiwanjeon <jeonjiwan94@gmail.com>
2022-02-09 09:27:01 +0900

    Edit: add product in settings.py INSTALLED_APPS part

--- twinny_practice/settings.py
+++ twinny_practice/settings.py
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders'
+    'corsheaders',
+    'product'
 ]
 
 MIDDLEWARE = [
